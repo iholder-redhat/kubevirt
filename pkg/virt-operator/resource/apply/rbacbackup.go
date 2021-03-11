a4f30940119271d5fb2ff4f430b199cb08315f2d
Itamar Holder <iholder@redhat.com>
2021-03-11 16:11:51 +0200

    [virt-operator]: Refactor - one instead of three duplicated functions
    The functions needsClusterRoleBackup, needsRoleBindingBackup, needsRoleBackup
    are almost completely duplicated. They are now replaced by one
    "needUpdate" function.
    
    Signed-off-by: Itamar Holder <iholder@redhat.com>

--- pkg/virt-operator/resource/apply/rbacbackup.go
+++ pkg/virt-operator/resource/apply/rbacbackup.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	"k8s.io/client-go/tools/cache"
+
 	rbacv1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	v1 "kubevirt.io/client-go/api/v1"
 	"kubevirt.io/client-go/log"
-	"kubevirt.io/kubevirt/pkg/virt-operator/util"
 )
 
 func (r *Reconciler) backupRbac() error {
@@ -20,7 +21,7 @@ func (r *Reconciler) backupRbac() error {
 	objects := r.stores.ClusterRoleCache.List()
 	for _, obj := range objects {
 		cachedCr, ok := obj.(*rbacv1.ClusterRole)
-		if !ok || !needsClusterRoleBackup(r.kv, r.stores, cachedCr) {
+		if !ok || !needsUpdate(r.kv, r.stores.ClusterRoleCache, &cachedCr.ObjectMeta) {
 			continue
 		}
 		imageTag, imageRegistry, id, ok := getInstallStrategyAnnotations(&cachedCr.ObjectMeta)
@@ -52,7 +53,7 @@ func (r *Reconciler) backupRbac() error {
 	objects = r.stores.ClusterRoleBindingCache.List()
 	for _, obj := range objects {
 		cachedCrb, ok := obj.(*rbacv1.ClusterRoleBinding)
-		if !ok || !needsClusterRoleBindingBackup(r.kv, r.stores, cachedCrb) {
+		if !ok || !needsUpdate(r.kv, r.stores.ClusterRoleBindingCache, &cachedCrb.ObjectMeta) {
 			continue
 		}
 		imageTag, imageRegistry, id, ok := getInstallStrategyAnnotations(&cachedCrb.ObjectMeta)
@@ -83,7 +84,7 @@ func (r *Reconciler) backupRbac() error {
 	objects = r.stores.RoleCache.List()
 	for _, obj := range objects {
 		cachedCr, ok := obj.(*rbacv1.Role)
-		if !ok || !needsRoleBackup(r.kv, r.stores, cachedCr) {
+		if !ok || !needsUpdate(r.kv, r.stores.RoleCache, &cachedCr.ObjectMeta) {
 			continue
 		}
 		imageTag, imageRegistry, id, ok := getInstallStrategyAnnotations(&cachedCr.ObjectMeta)
@@ -114,7 +115,7 @@ func (r *Reconciler) backupRbac() error {
 	objects = r.stores.RoleBindingCache.List()
 	for _, obj := range objects {
 		cachedRb, ok := obj.(*rbacv1.RoleBinding)
-		if !ok || !needsRoleBindingBackup(r.kv, r.stores, cachedRb) {
+		if !ok || !needsUpdate(r.kv, r.stores.RoleBindingCache, &cachedRb.ObjectMeta) {
 			continue
 		}
 		imageTag, imageRegistry, id, ok := getInstallStrategyAnnotations(&cachedRb.ObjectMeta)
@@ -144,113 +145,6 @@ func (r *Reconciler) backupRbac() error {
 	return nil
 }
 
-func needsClusterRoleBackup(kv *v1.KubeVirt, stores util.Stores, cr *rbacv1.ClusterRole) bool {
-	imageTag, imageRegistry, id, shouldBackup := shouldBackupRBACObject(kv, &cr.ObjectMeta)
-	if !shouldBackup {
-		return false
-	}
-
-	// loop through cache and determine if there's an ephemeral backup
-	// for this object already
-	objects := stores.ClusterRoleCache.List()
-	for _, obj := range objects {
-		cachedCr, ok := obj.(*rbacv1.ClusterRole)
-
-		if !ok ||
-			cachedCr.DeletionTimestamp != nil ||
-			cr.Annotations == nil {
-			continue
-		}
-
-		uid, ok := cachedCr.Annotations[v1.EphemeralBackupObject]
-		if !ok {
-			// this is not an ephemeral backup object
-			continue
-		}
-
-		if uid == string(cr.UID) && objectMatchesVersion(&cachedCr.ObjectMeta, imageTag, imageRegistry, id, kv.GetGeneration()) {
-			// found backup. UID matches and versions match
-			// note, it's possible for a single UID to have multiple backups with
-			// different versions
-			return false
-		}
-	}
-
-	return true
-}
-
-func needsRoleBindingBackup(kv *v1.KubeVirt, stores util.Stores, rb *rbacv1.RoleBinding) bool {
-
-	imageTag, imageRegistry, id, shouldBackup := shouldBackupRBACObject(kv, &rb.ObjectMeta)
-	if !shouldBackup {
-		return false
-	}
-
-	// loop through cache and determine if there's an ephemeral backup
-	// for this object already
-	objects := stores.RoleBindingCache.List()
-	for _, obj := range objects {
-		cachedRb, ok := obj.(*rbacv1.RoleBinding)
-
-		if !ok ||
-			cachedRb.DeletionTimestamp != nil ||
-			rb.Annotations == nil {
-			continue
-		}
-
-		uid, ok := cachedRb.Annotations[v1.EphemeralBackupObject]
-		if !ok {
-			// this is not an ephemeral backup object
-			continue
-		}
-
-		if uid == string(rb.UID) && objectMatchesVersion(&cachedRb.ObjectMeta, imageTag, imageRegistry, id, kv.GetGeneration()) {
-			// found backup. UID matches and versions match
-			// note, it's possible for a single UID to have multiple backups with
-			// different versions
-			return false
-		}
-	}
-
-	return true
-}
-
-func needsRoleBackup(kv *v1.KubeVirt, stores util.Stores, r *rbacv1.Role) bool {
-
-	imageTag, imageRegistry, id, shouldBackup := shouldBackupRBACObject(kv, &r.ObjectMeta)
-	if !shouldBackup {
-		return false
-	}
-
-	// loop through cache and determine if there's an ephemeral backup
-	// for this object already
-	objects := stores.RoleCache.List()
-	for _, obj := range objects {
-		cachedR, ok := obj.(*rbacv1.Role)
-
-		if !ok ||
-			cachedR.DeletionTimestamp != nil ||
-			r.Annotations == nil {
-			continue
-		}
-
-		uid, ok := cachedR.Annotations[v1.EphemeralBackupObject]
-		if !ok {
-			// this is not an ephemeral backup object
-			continue
-		}
-
-		if uid == string(r.UID) && objectMatchesVersion(&cachedR.ObjectMeta, imageTag, imageRegistry, id, kv.GetGeneration()) {
-			// found backup. UID matches and versions match
-			// note, it's possible for a single UID to have multiple backups with
-			// different versions
-			return false
-		}
-	}
-
-	return true
-}
-
 func shouldBackupRBACObject(kv *v1.KubeVirt, objectMeta *metav1.ObjectMeta) (imageTag, imageRegistry, id string, shouldBackup bool) {
 	curVersion, curImageRegistry, curID := getTargetVersionRegistryID(kv)
 	shouldBackup = false
@@ -277,42 +171,6 @@ func shouldBackupRBACObject(kv *v1.KubeVirt, objectMeta *metav1.ObjectMeta) (ima
 
 }
 
-func needsClusterRoleBindingBackup(kv *v1.KubeVirt, stores util.Stores, crb *rbacv1.ClusterRoleBinding) bool {
-
-	imageTag, imageRegistry, id, shouldBackup := shouldBackupRBACObject(kv, &crb.ObjectMeta)
-	if !shouldBackup {
-		return false
-	}
-
-	// loop through cache and determine if there's an ephemeral backup
-	// for this object already
-	objects := stores.ClusterRoleBindingCache.List()
-	for _, obj := range objects {
-		cachedCrb, ok := obj.(*rbacv1.ClusterRoleBinding)
-
-		if !ok ||
-			cachedCrb.DeletionTimestamp != nil ||
-			crb.Annotations == nil {
-			continue
-		}
-
-		uid, ok := cachedCrb.Annotations[v1.EphemeralBackupObject]
-		if !ok {
-			// this is not an ephemeral backup object
-			continue
-		}
-
-		if uid == string(crb.UID) && objectMatchesVersion(&cachedCrb.ObjectMeta, imageTag, imageRegistry, id, kv.GetGeneration()) {
-			// found backup. UID matches and versions match
-			// note, it's possible for a single UID to have multiple backups with
-			// different versions
-			return false
-		}
-	}
-
-	return true
-}
-
 func getInstallStrategyAnnotations(meta *metav1.ObjectMeta) (imageTag, imageRegistry, id string, ok bool) {
 	ok = false
 
@@ -331,4 +189,39 @@ func getInstallStrategyAnnotations(meta *metav1.ObjectMeta) (imageTag, imageRegi
 
 	ok = true
 	return
+}
+
+func needsUpdate(kv *v1.KubeVirt, cache cache.Store, meta *metav1.ObjectMeta) bool {
+	imageTag, imageRegistry, id, shouldBackup := shouldBackupRBACObject(kv, meta)
+	if !shouldBackup {
+		return false
+	}
+
+	// loop through cache and determine if there's an ephemeral backup
+	// for this object already
+	objects := cache.List()
+	for _, obj := range objects {
+		cachedObj, ok := obj.(*metav1.ObjectMeta)
+
+		if !ok ||
+			cachedObj.DeletionTimestamp != nil ||
+			meta.Annotations == nil {
+			continue
+		}
+
+		uid, ok := cachedObj.Annotations[v1.EphemeralBackupObject]
+		if !ok {
+			// this is not an ephemeral backup object
+			continue
+		}
+
+		if uid == string(meta.UID) && objectMatchesVersion(cachedObj, imageTag, imageRegistry, id, kv.GetGeneration()) {
+			// found backup. UID matches and versions match
+			// note, it's possible for a single UID to have multiple backups with
+			// different versions
+			return false
+		}
+	}
+
+	return true
 }
